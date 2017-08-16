3a16a33f5c94049c23af147071639c963526517f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-08-16 21:56:08 +0900

    fix

--- Tests/RegexSwiftTests/RegexSwiftTests.swift
+++ Tests/RegexSwiftTests/RegexSwiftTests.swift
@@ -3,31 +3,34 @@ import XCTest
 
 class RegexSwiftTests: XCTestCase {
     
-    func testMatchExample() {
-        
-    }
-    
     func testCompileExample() {
-        
+        let _ = RegexSwift.mustCompile(pattern: "a*c")
     }
     
-    func tesetReplaceAllStringExample() {
-        
+    func testMatchExample() {
+        let regex = RegexSwift.mustCompile(pattern: "a*c")
+        print(regex.match(target: "aabbbbbbbc"))
     }
     
-    func testFindAllStringSubMatchExample() {
-        
+    func testReplaceAllStringExample() {
+        let regex = RegexSwift.mustCompile(pattern: "[A-Za-z]*right")
+        print(regex.replaceAllString(target:  "Copyright 2017 hlts." , with: "Copyleft"))
     }
     
     func testSplitExample() {
-        
+        let regex = RegexSwift.mustCompile(pattern: "\\s*,\\s")
+        print(regex.split(target: "00000001,hihihihi roooootototototo, hlts2")!)
+    }
+    
+    func testFindAllStringSubMatchExample() {
+        let regex = RegexSwift.mustCompile(pattern: "[\\d\\-]+")
+        print(regex.findAllStringSubMatch(target: "hiro:123-111-222245 hirot:004123455-33333222-245354234222")!)
     }
     
     static var allTests = [
-        ("testMatchExample", testMatchExample),
         ("testCompileExample", testCompileExample),
-        ("tesetReplaceAllStringExample", tesetReplaceAllStringExample),
-        ("testFindAllStringSubMatchExample", testFindAllStringSubMatchExample),
-        ("testSplitExample", testSplitExample)
+        ("testMatchExample", testMatchExample),
+        ("testReplaceAllStringExample", testReplaceAllStringExample),
+        ("testFindAllStringSubMatchExample", testFindAllStringSubMatchExample)
     ]
 }
